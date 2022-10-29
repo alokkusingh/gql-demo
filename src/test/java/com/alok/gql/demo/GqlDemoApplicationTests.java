45a389b0cf05ff10204445785d6acc8456a5f4ab
alokkusingh <alok.ku.singh@gmail.com>
2022-10-29 23:41:06 +0530

    first commit

---
+++ src/test/java/com/alok/gql/demo/GqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alok.gql.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
